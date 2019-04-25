25c082869d0ded6ea1c6505af0272b27b2c96c29
Adriano Avelino <adriano-st777@hotmail.com>
2019-04-24 22:17:41 -0300

    adds index test to TeamsController

---
+++ spec/controllers/teams_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe TeamsController, type: :controller do
+  include Devise::Test::ControllerHelpers
+
+  before(:each) do
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+    @current_user = FactoryBot.create(:user)
+    sign_in @current_user
+  end
+
+  describe "GET #index" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
